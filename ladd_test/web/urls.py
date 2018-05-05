b36c0ebd78bb61c568295b4331085eed426ef9eb
laddin <laddin@163.com>
2018-05-06 06:51:37 +0800

    2

---
+++ ladd_test/web/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('<int:web_id>', views.web_detail,name="web_detail"),
+
+    ]
--- ladd_test/web/views.py
+++ ladd_test/web/views.py
@@ -1,3 +1,12 @@
+from django.shortcuts import render_to_response, get_object_or_404
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import web_info
+
 
 # Create your views here.
+
+def web_detail(request, web_id):
+    web = get_object_or_404(web_info, pk=web_id)
+
+    return HttpResponse(web.web_name)
