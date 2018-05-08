f7495134dd25a7bb34bff109c87505c3772d0fe0
laddin <pengfei502k@163.com>
2018-05-08 19:09:21 +0800

    add test html

--- ladd_test/web/views.py
+++ ladd_test/web/views.py
@@ -10,6 +10,14 @@ def web_detail(request, web_id):
     web = get_object_or_404(web_info, pk=web_id)
     return HttpResponse(web.web_name)
 
-def test(request):
-    aaa = web_info.objects.all()
-    return render(request,"web/index.html",context={'post_list':aaa})
+def index(request):
+    # aaa = web_info.objects.all()
+    return render(request, "web/index.html")
+
+
+def today(request):
+    return render(request, "web/today.html")
+
+
+def history(request):
+    return render(request, "web/history.html")
