24d4bd98d50924b051a520ebc0c4b71d6597ac16
laddin <pengfei502k@163.com>
2018-05-08 19:08:43 +0800

    add url

--- ladd_test/web/urls.py
+++ ladd_test/web/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('<int:web_id>', views.web_detail, name="web_detail"),
-    path('', views.test)
+    path('', views.index),
+    path('index.html',views.index),
+    path('today.html', views.today),
+    path('history.html', views.history),
 ]
