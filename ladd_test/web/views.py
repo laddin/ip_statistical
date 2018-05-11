325db1364b23f6fbf54a74de4a3c46bcc9963cf7
laddin <pengfei502k@163.com>
2018-05-11 18:59:40 +0800

    测试数据

--- ladd_test/web/views.py
+++ ladd_test/web/views.py
@@ -15,10 +15,21 @@ def index(request):
     # aaa = web_info.objects.all()
     return render(request, "web/index.html")
 
-
 def today(request):
-    data = "当日数据 主页"
-    return render(request, "web/today.html", {'data': data})
+    today_date = timezone.now().strftime("%Y-%m-%d")
+    # print(today_date,type(today_date))
+    http_name = ['web1', 'web2', 'web3', 'web4', 'web1', 'web2', 'web3', 'web4', 'web1', 'web2', 'web3', 'web4', 'web1',
+                 'web2', 'web3', 'web4']
+    today_ip_num = [11, 22, 3, 44, 11, 22, 3, 44, 11, 22, 3, 44, 11, 22, 3, 44]
+    today_spider_num = [1231, 12412, 1231, 123, 5235, 123, 52341, 124, 54, 213, 1231, 12412, 1231, 123, 5235, 123,
+                        52341, 124, 54, 213]
+    today_record_num = [123, 2341, 43, 36, 213, 123, 12, 312]
+    return render(request, "web/today.html", {'http_name': http_name,
+                                              'today_date': today_date,
+                                              'today_ip_num': today_ip_num,
+                                              'today_spider_num': today_spider_num,
+                                              'today_record_num': today_record_num,
+                                              })
 
 
 def history(request):
